79a7c79e503a981e3b734c7cd6dcfc26eacfdab6
fanchengkong21 <kfc1301478241@163.com>
2016-10-26 10:31:52 +0800

    2016年10月26日10:31

--- app/src/main/java/com/kfc/gitdemo/MainActivity.java
+++ app/src/main/java/com/kfc/gitdemo/MainActivity.java
@@ -4,21 +4,30 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button button;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         button = ((Button) findViewById(R.id.button));
+        textView= ((TextView) findViewById(R.id.textview));
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(MainActivity.this, "不错", Toast.LENGTH_SHORT).show();
+            }
+        });
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "还可以", Toast.LENGTH_SHORT).show();
             }
         });
     }
